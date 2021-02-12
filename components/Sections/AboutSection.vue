4a6fc57c3e56c3d14102453255de9d32b395a1ed
Alexandr Borecky <alexandrborecky@Alexandrs-MacBook-Pro.local>
2021-02-12 17:39:46 +0100

    og set

--- components/Sections/AboutSection.vue
+++ components/Sections/AboutSection.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="hero flex middle" :style="{backgroundColor: color}">
         <div class="container">
+            <img :src="image1" alt="">
+            <img :src="image2" alt="">
+            <img :src="image3" alt="">
+            <img :src="image4" alt="">
             <p class="big full-width">
                 {{text}}
             </p>
@@ -13,7 +17,11 @@
         name: 'AboutSection',
         props: [
             'text',
-            'color'
+            'color',
+            'image1',
+            'image2',
+            'image3',
+            'image4',
         ]
     }
 </script>
@@ -22,6 +30,35 @@
 
 .hero {
     background-color: $pod-purple;
+    .container {
+        img {
+            position: absolute;
+            z-index: 1 !important;
+            max-width: 240px;
+            &:first-child {
+                top: 5%;
+                max-width: 120px;
+            }
+            &:nth-child(2) {
+                top: 100px;
+                right: 10%;
+            }
+            &:nth-child(3) {
+                bottom: 5%;
+            }
+            &:nth-child(4) {
+                bottom: 5%;
+                right: 15%;
+                max-width: 120px;
+            }
+        }
+        p {
+            z-index: 100 !important;
+             @media only screen and (max-width: 550px) {
+                margin: 40px 0;
+            }
+        }
+    }
 }
 
 </style>
