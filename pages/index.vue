053b97c3cd0c9dce47c9977387236dc7d1160fa2
Alexandr Borecky <alexandrborecky@Alexandrs-MacBook-Pro.local>
2021-06-22 19:11:07 +0200

    v 1.0

--- pages/index.vue
+++ pages/index.vue
@@ -7,8 +7,8 @@
         text="Když v USA zakládala Reese Witherspoon své společnosti, které produkovaly filmy s hlavními ženskými hrdinkami, bylo to proto, že jako herečka chtěla hrát lépe napsané, zajímavé role. Zároveň však jako podnikatelka musela tušit, že jsou to právě tyto filmy, které v příštích letech oslaví nejen umělecký úspěch. V České republice tento trend ještě neexistuje. Je však nejen pro společnost prospěšné, ale i finančně výhodné být součástí této změny narativu. Jsme proto velmi vděční za investory i talenty, se kterými jsme navázali a navážeme spolupráci. "
       />
       <TextSection id="hudba"
-        title="Hudební a marketingová divize"
-        text="Spolupracujeme se začínajícími i známými umělci, se kterými konzultujeme jejich umělecké nasměrování a zároveň je propojujeme se značkami. Často se jedná o multitalentované umělce, kterým pomáháme navigovat jejich cestu vstříc obory. ANNA Entertainment spolupracuje se zpěvačkou, infuencerkou a zakladatelkou WOW WOW Collective Gabi Heclovou, s projektem Hills 97 zpěváka a herce Adama Mišíka a  producenta enthica, a také s herečkou, zpěvačkou a influencerkou Marianou Prachařovou. Ze začínajících umělců zastupujeme například kapelu DOMI v čele se zpěvačkou Dominikou Haškovou."
+        title="Umělecký management a marketing"
+        text="Spolupracujeme se začínajícími i známými umělci, převážně s hudebníky, se kterými konzultujeme jejich umělecké nasměrování a zároveň je propojujeme se značkami. Často se jedná o multitalentované umělce, kterým pomáháme navigovat jejich cestu vstříc obory. ANNA Entertainment spolupracuje se zpěvačkou, infuencerkou a zakladatelkou WOW WOW Collective Gabi Heclovou a také s herečkou, zpěvačkou a influencerkou Marianou Prachařovou."
       />
       <about-section id="o-nas"/>
       <Quotes id="quotes" />
